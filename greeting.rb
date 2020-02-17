2bb82435488a544f524a05ffa5ac62a876a353b5
Mike Pottebaum <mpottebaum@gmail.com>
2020-02-17 02:42:12 +0000

    Automatically backed up by Learn

---
+++ greeting.rb
@@ -0,0 +1,11 @@
+def greeting
+  puts "Hello World"
+end
+
+def say_greeting_five_times
+  5.times do
+    greeting
+  end
+end
+
+say_greeting_five_times
---
+++ vending_machine.rb
@@ -0,0 +1,11 @@
+def product_maker(name, price)
+  {:name => name, :price => price}
+end
+
+vending_machine = [
+  [product_maker("Regular Chips", 1), product_maker("Barbecue Chips", 1), ], product_maker("Salt and Vinegar Chips", 1), product_maker("Vanilla Cookies", 3), product_maker("Pistachio Cookies", 3), product_maker("Chocolate Cookies", 3)],
+  [product_maker("Regular Chips", 1), product_maker("Barbecue Chips", 1), ], product_maker("Salt and Vinegar Chips", 1), product_maker("Vanilla Cookies", 3), product_maker("Pistachio Cookies", 3), product_maker("Chocolate Cookies", 3)],
+  [product_maker("Regular Chips", 1), product_maker("Barbecue Chips", 1), ], product_maker("Salt and Vinegar Chips", 1), product_maker("Vanilla Cookies", 3), product_maker("Pistachio Cookies", 3), product_maker("Chocolate Cookies", 3)],
+  [product_maker("Regular Chips", 1), product_maker("Barbecue Chips", 1), ], product_maker("Salt and Vinegar Chips", 1), product_maker("Vanilla Cookies", 3), product_maker("Pistachio Cookies", 3), product_maker("Chocolate Cookies", 3)],
+  [product_maker("Regular Chips", 1), product_maker("Barbecue Chips", 1), ], product_maker("Salt and Vinegar Chips", 1), product_maker("Vanilla Cookies", 3), product_maker("Pistachio Cookies", 3), product_maker("Chocolate Cookies", 3)],
+  ]
